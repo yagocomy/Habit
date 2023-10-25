7267d78fa646e9b8a9ebfa9f4c752c9d90ea4337
Yago <yagodemourasilva@gmail.com>
2023-10-25 12:43:00 -0300

    Initial Commit

---
+++ Habit/HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitApp.swift
+//  Habit
+//
+//  Created by Yago De Moura Silva on 25/10/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
